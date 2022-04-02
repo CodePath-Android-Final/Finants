074752698f8362cb090f040d27ca84c9049a470c
anthonypoon12 <anthony@poons.us>
2022-04-01 23:35:09 -0400

    Formatting Fixes

--- app/src/main/java/com/example/finants/PostAdapter.kt
+++ app/src/main/java/com/example/finants/PostAdapter.kt
@@ -1,6 +1,10 @@
 package com.example.finants
 
 import android.content.Context
+import android.graphics.Color
+import android.text.SpannableString
+import android.text.SpannableStringBuilder
+import android.text.style.ForegroundColorSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -33,7 +37,19 @@ class PostAdapter(val context: Context, val posts: List<Post>)
             tvDescription = itemView.findViewById(R.id.tvDescription)
         }
         fun bind(post: Post){
-            tvDescription.text = post.getGoal()
+            val builder: SpannableStringBuilder  = SpannableStringBuilder()
+            val str1: SpannableString = SpannableString(post.getUser()?.username)
+            str1.setSpan(ForegroundColorSpan(Color.BLUE), 0, str1.length, 0)
+            builder.append(str1)
+            builder.append(" wants to save up $")
+            val str2: SpannableString = SpannableString(post.getAmount().toString())
+            str2.setSpan(ForegroundColorSpan(Color.GREEN),0,str2.length,0)
+            builder.append(str2)
+            builder.append(" for ")
+            val str3: SpannableString = SpannableString(post.getGoal())
+            str3.setSpan(ForegroundColorSpan(Color.RED),0,str3.length,0)
+            builder.append(str3)
+            tvDescription.setText(builder, TextView.BufferType.SPANNABLE)
             Glide.with(itemView.context).load(post.getUser()?.getParseFile("profile_pic")?.url).into(ivProfilePicture)
         }
     }
